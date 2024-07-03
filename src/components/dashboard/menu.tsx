af62219015b4a63107daf09d2047343c0a13ef5d
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-07-02 22:20:02 -0400

    typescript :)

--- src/components/dashboard/menu.tsx
+++ src/components/dashboard/menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Ellipsis, LogOut } from "lucide-react";
+import { Ellipsis, LogOut, LucideIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
@@ -51,55 +51,59 @@ export function Menu({ isOpen }: MenuProps) {
                 <p className="pb-2"></p>
               )}
               {menus.map(
-                ({ href, label, icon: Icon, active, submenus }, index) =>
-                  submenus.length === 0 ? (
-                    <div className="w-full" key={index}>
-                      <TooltipProvider disableHoverableContent>
-                        <Tooltip delayDuration={100}>
-                          <TooltipTrigger asChild>
-                            <Button
-                              variant={active ? "secondary" : "ghost"}
-                              className="w-full justify-start h-10 mb-1"
-                              asChild
-                            >
-                              <Link href={href}>
-                                <span
-                                  className={cn(isOpen === false ? "" : "mr-4")}
-                                >
-                                  <Icon size={18} />
-                                </span>
-                                <p
-                                  className={cn(
-                                    "max-w-[200px] truncate",
-                                    isOpen === false
-                                      ? "-translate-x-96 opacity-0"
-                                      : "translate-x-0 opacity-100"
-                                  )}
-                                >
-                                  {label}
-                                </p>
-                              </Link>
-                            </Button>
-                          </TooltipTrigger>
-                          {isOpen === false && (
-                            <TooltipContent side="right">
-                              {label}
-                            </TooltipContent>
-                          )}
-                        </Tooltip>
-                      </TooltipProvider>
-                    </div>
-                  ) : (
-                    <div className="w-full" key={index}>
-                      <CollapseMenuButton
-                        icon={Icon}
-                        label={label}
-                        active={active}
-                        submenus={submenus}
-                        isOpen={isOpen}
-                      />
-                    </div>
+                ({ href, label, icon: Icon1, active, submenus }, index) => {
+                 const Icon: LucideIcon = Icon1 as LucideIcon;
+                  return (
+                    submenus.length === 0 ? (
+                      <div className="w-full" key={index}>
+                        <TooltipProvider disableHoverableContent>
+                          <Tooltip delayDuration={100}>
+                            <TooltipTrigger asChild>
+                              <Button
+                                variant={active ? "secondary" : "ghost"}
+                                className="w-full justify-start h-10 mb-1"
+                                asChild
+                              >
+                                <Link href={href}>
+                                  <span
+                                    className={cn(isOpen === false ? "" : "mr-4")}
+                                  >
+                                    <Icon size={18} />
+                                  </span>
+                                  <p
+                                    className={cn(
+                                      "max-w-[200px] truncate",
+                                      isOpen === false
+                                        ? "-translate-x-96 opacity-0"
+                                        : "translate-x-0 opacity-100"
+                                    )}
+                                  >
+                                    {label}
+                                  </p>
+                                </Link>
+                              </Button>
+                            </TooltipTrigger>
+                            {isOpen === false && (
+                              <TooltipContent side="right">
+                                {label}
+                              </TooltipContent>
+                            )}
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                    ) : (
+                      <div className="w-full" key={index}>
+                        <CollapseMenuButton
+                          icon={Icon}
+                          label={label}
+                          active={active}
+                          submenus={submenus}
+                          isOpen={isOpen}
+                        />
+                      </div>
+                    )
                   )
+                }
               )}
             </li>
           ))}
